9c20b95bee27c1f462c3353b7abc2ea1faa565c6
hom237 <jinsic4568@gmail.com>
2024-10-29 19:18:43 +0900

    :tada:
    start kook android

---
+++ app/src/main/java/com/hacker/thone/kook/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.hacker.thone.kook
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hacker.thone.kook.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/hacker/thone/kook/TakingVideoFragment.kt
@@ -0,0 +1,27 @@
+package com.hacker.thone.kook
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hacker.thone.kook.databinding.FragmentTakingVideoBinding
+
+class TakingVideoFragment : Fragment() {
+
+    private var _binding  : FragmentTakingVideoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTakingVideoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
