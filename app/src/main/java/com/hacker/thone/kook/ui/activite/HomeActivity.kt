7ffdd8353f1edba72ab8b2ebbca03ee166abff83
hom237 <jinsic4568@gmail.com>
2024-10-29 20:02:49 +0900

    :lipstick: init main activity

---
+++ app/src/main/java/com/hacker/thone/kook/ui/activite/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.hacker.thone.kook.ui.activite
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hacker.thone.kook.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
